30baa4f53f810cab2e86138197fc192080267a4a
TrinityX <alexrsagen@gmail.com>
2014-08-08 01:06:47 +0200

    Improved routing, less code, and allows you to set a page root.

--- routes.js
+++ routes.js
@@ -1,24 +1,28 @@
-// Extra routes go below this line (for example all app.get's)
+/*  All extra routing goes below this comment, example:
+
+	app.get("/test", function (req, res) {
+		res.send('test');
+	});
+*/
 
 // Routing
 module.exports = function(app) {
 	var requireDir = require('require-dir');
 	var routes = requireDir('./routes', {recurse: true});
-	//console.log(routes);
 
 	// Include index page
 	app.use("/", routes['index']);
 
 	// Recurse through routes and include them
-	function addRoute(routes, path) {
-		for(route in routes) {
-			if(typeof routes[route] === 'function') {
-				app.use(path + route, routes[route]);
-			} else {
-				addRoute(routes[route], path + route + '/');
-			}
-		}
+	function useOrRecurse(routes, path) {
+		if(typeof routes === 'object')
+			Object.keys(routes).forEach(function(key) {
+				useOrRecurse(routes[key], path + key + '/');
+			});
+		else
+			app.use(path, routes);
 	}
 
-	addRoute(routes, "/");
+	// If you replace the / below with for example /pages/, all pages will be under /pages/
+	useOrRecurse(routes, "/");
 }
